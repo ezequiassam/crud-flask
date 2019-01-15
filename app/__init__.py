58a16c35c7aaf153cb981ec601bc0ac0d54fb548
ezequiassam <ezequiassam@hotmail.com>
2019-01-15 10:37:03 -0300

    adicionando configurações do banco

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config.from_pyfile('config.py')
+
 db = SQLAlchemy(app)
+migrade = Migrate(app, db)
 
+manager = Manager(app)
+# manager.app_command('db', MigrateCommand)
+
+from app.models import tables
 from app.controllers import default
-
